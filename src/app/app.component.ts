3599b78c495d8bb1ed3551002f41804f1427c899
josepereza <josepereza66@gmail.com>
2020-09-24 19:27:31 +0200

    first2 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import {AfterViewInit, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { fromEvent } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit  {
   title = 'ejemplofromevent';
+  @ViewChild('hola') button: ElementRef;
+  ngAfterViewInit() {
+    const mouseMoves = fromEvent(this.button.nativeElement, 'click')
+      .subscribe((res:any) => console.log(`Coords: ${res.clientX} X ${res.clientY}`));
+
+  }
 }
